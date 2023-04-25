84a3677f6880f3ef150d4007f5936761257a86bf
joseph <josephgallo97@gmail.com>
2023-04-25 16:56:36 +0200

    added main fragment (layout and .kt file)

--- app/src/main/java/ch/hslu/mobpro/mypokedex/main/MainActivity.kt
+++ app/src/main/java/ch/hslu/mobpro/mypokedex/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package ch.hslu.mobpro.mypokedex
+package ch.hslu.mobpro.mypokedex.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ch.hslu.mobpro.mypokedex.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ch/hslu/mobpro/mypokedex/main/MainFragment.kt
@@ -0,0 +1,65 @@
+package ch.hslu.mobpro.mypokedex.main
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import ch.hslu.mobpro.mypokedex.R
+import kotlin.random.Random
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainPokedexBinding? = null
+
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    private var dialogBuilder: AlertDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        var randomNr = Random.nextInt(1, 152)
+
+        Picasso.get()
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$randomNr.png")
+            .into(binding.pokemonHeaderImage)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMainPokedexBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    //MAIN CODE
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.pokedexButton.setOnClickListener {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, PokedexFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
